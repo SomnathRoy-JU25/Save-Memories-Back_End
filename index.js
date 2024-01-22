a28e253a0e080b080d74c55e810cca83de51a84d
Somnath <107599122+SomnathRoy-JU25@users.noreply.github.com>
2024-01-22 10:22:09 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/api/v1/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://somnathroy9564:4WudmHjiMUMLeJ2G@cluster0.pr7azmw.mongodb.net/memoriesAppDB';
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
